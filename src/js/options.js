c06f497d01455c76082cad5fd06d2211e1c2a373
Joseph <joseph@verifiedjoseph.com>
2017-05-03 02:03:44 +0100

    Renamed code folder to src and added updated code (v2.2)
    Renamed folder to src and added updated code (v2.2)
    Removed change.log and version.log

---
+++ src/js/options.js
@@ -0,0 +1,69 @@
+/*jslint node: true */
+/*global chrome */
+"use strict";
+
+(function () {
+	
+	var status = document.getElementById("status");
+	
+	/* 
+		Check api type (chrome (based. e.g: opera), Firefox or MS Edge)
+	*/
+	if (typeof chrome !== 'undefined' && typeof browser === 'undefined') {
+		var browser = chrome;
+	}
+	
+	/*
+		Save option to storage
+	*/
+	function save_options() {
+		var channel_one = document.getElementById('channel_1').value,
+			channel_two = document.getElementById('channel_2').value;
+		
+		if (channel_one === '' && channel_two === '') { // No channels entered	
+			status.textContent = "Enter a channel name or ID!";
+			
+			setTimeout(function () {
+				status.textContent = "";
+			}, 1750);
+		
+		} else {
+			
+			browser.storage.sync.set({
+				channel_one: channel_one,
+				channel_two: channel_two
+			}, function () {
+	
+				// Update status to let user know options were saved.
+				status.textContent = 'Options saved.';
+				
+				setTimeout(function () {
+					status.textContent = '';
+				}, 1750);
+				
+			});
+			
+		}
+	}
+	
+	/*
+		Load and display current options
+	*/
+	browser.storage.sync.get({
+		// Use default values
+		channel_one: '',
+		channel_two: ''
+	}, function (items) {
+		console.log(items);
+
+		document.getElementById('channel_1').value = items.channel_one;
+		document.getElementById('channel_2').value = items.channel_two;
+		
+	});
+	
+	/* 
+		Event listener for saving options
+	*/
+	document.getElementById('save').addEventListener('click', save_options);
+
+}());
---
+++ src/js/popup.js
@@ -0,0 +1,119 @@
+/*jslint node: true */
+/*global chrome */
+"use strict";
+
+(function () {
+	
+	/* 
+		Check api type (chrome (based. e.g: opera), Firefox or MS Edge)
+	*/
+	if (typeof chrome !== 'undefined' && typeof browser === 'undefined') {
+		var browser = chrome;
+	}
+	
+	/*
+		Create a Iframe
+	*/
+	function createIframe(channel) {
+		
+		var div = document.createElement('div'),
+			divShow = document.createElement('div'),
+			iframe = document.createElement('iframe'),
+			img = document.createElement('img'),
+			a = document.createElement('a');
+		
+		// Img 1px
+		img.className = 'iframe-link';
+		img.setAttribute('src', '../images/1px.png');
+		img.setAttribute('title', 'Click to go to stats page');
+		a.appendChild(img);
+
+		// a href
+		a.href = 'http://socialblade.com/youtube/user/' + channel;
+		a.setAttribute('target', '_blank');
+		a.className = 'iframe-title-a';
+		div.appendChild(a);
+		
+		// Div Show
+		divShow.className = 'iframe-show';
+		div.appendChild(divShow);
+		
+		// Div
+		div.className = 'iframe';
+		
+		// Iframe
+		iframe.setAttribute('src', 'http://widget.socialblade.com/widget.php?u=' + channel);
+		iframe.style.width = '220px';
+
+		 // Set diferent iframe height for Firefox (http://stackoverflow.com/a/26358856)
+		if (navigator.userAgent.indexOf("Firefox") !== -1) {
+			iframe.style.height = '130px';
+			
+		} else { // All other browsers
+			iframe.style.height = '116px';
+		}
+		
+		iframe.style.border = '0';
+		iframe.style.overflow = 'hidden';
+		iframe.scrolling = 'no';
+		divShow.appendChild(iframe);
+		
+		document.getElementById('iframe-wrap').appendChild(div);
+ 
+	}
+	
+	/*
+		Display iframes
+	*/
+	function display(items) {
+		
+		if (items.channel_one === '' && items.channel_two === '') {
+			document.getElementById('error').style.display = 'block';
+			
+			document.getElementById('options-e').addEventListener('click', function () {
+
+				window.open(chrome.runtime.getURL('html/options.html'));
+		
+			});
+			
+		} else {
+			
+			if (items.channel_one !== '') {
+				createIframe(items.channel_one);
+			}
+			
+			if (items.channel_two !== '') {
+				createIframe(items.channel_two);
+			}
+
+		}
+		
+	}
+	
+	/*
+		Fetch channels from storage
+	*/
+	browser.storage.sync.get({
+		// Use default values
+		channel_one: '',
+		channel_two: ''
+	}, function (items) {
+
+		display(items);
+	
+		if (browser.runtime.lastError) {
+			console.log(browser.runtime.lastError);
+		}
+		
+	});
+	
+	/* 
+		Event listener for opening options page
+	*/
+	document.getElementById('options').addEventListener('click', function () {
+
+		window.open(browser.runtime.getURL('html/options.html'));
+
+	});
+
+}());
